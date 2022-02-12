e308c134c4bee12dc83204db5aade09128c35d77
johnflorida2022 <jrosales@my365.bellevue.edu>
2022-02-11 21:04:22 -0500

    Update peerReviewErrors.py

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -5,8 +5,7 @@
 # Creation Date: 02/11/22
 # Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
 
-import random
-import time
+
 
 def displayIntro():
 	print('''You are in a land full of dragons. In front of you,
@@ -45,7 +44,6 @@ while playAgain == 'yes' or playAgain == 'y':
 	displayIntro()
 	caveNumber = choosecave()
 	checkCave(caveNumber)
-    
 	print('Do you want to play again? (yes or no)')
 	playAgain = input()
 	if playAgain == "no":
